8d322e7c14ea016bb2ff9ec91e2373dfb64438d3
Diego Contreras <diegocontreras401@gmail.com>
2025-11-21 12:37:40 -0600

    added home button at bottom of page to scroll back up to top of page

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let recommendationBoxes = document.querySelector('.recommendation-boxes'); 
 
 recommendationBoxes.innerHTML = `
-    <p class="bubble-text">" Working with Diego has been a smooth and easy process. He is adaptable and always trust worthy "</p>
-    <p class="bubble-text">" Hi my name is Diego "</p>
-    <p class="bubble-text">" My girlfriends name is Claire "</p>
+    <p class="bubble-text">" Working with Diego has been a smooth and easy process. "</p>
+    <p class="bubble-text">" Diego is adaptable and always trust worthy  "</p>
+    <p class="bubble-text">" Diego provides great service "</p>
 `
 
 const submitBtn = document.querySelector('.recommendation-button'); 
@@ -11,9 +11,22 @@ const submitBtn = document.querySelector('.recommendation-button');
 submitBtn.addEventListener('click', addRecommendation); 
 
 function addRecommendation() {
-    let recommendationInput = document.querySelector('.recommendation-input'); 
+    let recommendationInput = document.querySelector('.recommendation-input');
 
     recommendationBoxes.innerHTML += `<p class="bubble-text">" ${recommendationInput.value} "</p>`; 
 
     recommendationInput.value = ''; 
+
+    alert('Thank you for submitting a recommendation!')
+}
+
+const homeBtn = document.querySelector('.home-btn'); 
+
+homeBtn.addEventListener('click', scrollHome); 
+
+function scrollHome() {
+    window.scrollTo({
+        top: 0, 
+        behavior: 'smooth'
+    }); 
 }
